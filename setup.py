99b39cf1ad3c3ce2f5bf3cbb9c5411d58221f383
rfukutome <rfukutome@gmail.com>
2017-02-06 01:01:11 -0800

    Starting to work on an aerospace calculator.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='aerospace_calculator',
+      version='0.1',
+      description='Aerospace Calculator for basic and advanced calculations',
+      url='https://github.com/rfukutome/aerospace-calculator.git',
+      author='Richard Fukutome',
+      author_email='rfukutome@gmail.com',
+      license='MIT',
+      packages=['aerospace_calculator'],
+      zip_safe=False)
